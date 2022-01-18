e040e29a748c1b3eb2f84a0f97eb74cb0231d4e3
Megan Lukes@gmail.com <meglukes@gmail.com>
2022-01-18 12:56:16 -0500

    add ball movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("medium violet red")
@@ -7,15 +9,21 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle_left = Paddle()
-paddle_right = Paddle()
+paddle_left = Paddle((-350, 0))
+paddle_right = Paddle((350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(paddle_right.go_up, "Up")
 screen.onkey(paddle_right.go_down, "Down")
+screen.onkey(paddle_left.go_up, "w")
+screen.onkey(paddle_left.go_down, "s")
 
 playing = True
+
 while playing:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,11 +1,11 @@
-from turtle import Screen, Turtle
+from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.penup()
-        self.goto(350, 0)
+        self.goto(position)
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
